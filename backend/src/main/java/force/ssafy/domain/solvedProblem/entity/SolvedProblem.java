480771ec2b083bff9da59e803f7a2539084e4e24
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2025-05-08 23:51:27 +0900

    feat: 엔티티 인덱싱 추가
    - 인덱싱 멤버+문제,멤버+해결한날짜, 해결한날짜 3가지의 인덱싱 추가
    - 첫번째로 푼 문제인지 알려주는 플래그 필드 추가

--- backend/src/main/java/force/ssafy/domain/solvedProblem/entity/SolvedProblem.java
+++ backend/src/main/java/force/ssafy/domain/solvedProblem/entity/SolvedProblem.java
@@ -5,21 +5,33 @@ import force.ssafy.domain.problem.entity.Problem;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "solved_problem", indexes = {
+        @Index(name = "idx_member_problem", columnList = "member_id, problem_id"),
+        @Index(name = "idx_member_solved_date", columnList = "member_id, solved_date"),
+        @Index(name = "idx_solved_date", columnList = "solved_date")
+})
 public class SolvedProblem {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="member_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="problem_id", nullable = false)
+    @JoinColumn(name = "problem_id", nullable = false)
     private Problem problem;
     private LocalDateTime solvedDate;
     @Enumerated(EnumType.STRING)
@@ -27,4 +39,5 @@ public class SolvedProblem {
     private int timeComplexity;
     private int spaceComplexity;
     private String submitUrl;
+    private Boolean isFirstSolved;
 }
