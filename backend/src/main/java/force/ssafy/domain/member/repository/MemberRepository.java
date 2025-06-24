66dbcffb74a0b06d6e84bd9b2a684b8301e414ae
blueconecell <kimjeongyeon113@gmail.com>
2025-06-24 16:49:14 +0900

    feat: 배치 프로그램 개발 - 전체 유저 중 검증된 유저만(솔브닥 인증) 골라서 해결한 문제 동기화 - 한번에 10명을 1개의 청크로 관리 - 3번의 오류가 나오기 전까지는 계속 작업 진행 - 3번의 오류가 나오는 시점에 배치 프로그램 중단

--- backend/src/main/java/force/ssafy/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/force/ssafy/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package force.ssafy.domain.member.repository;
 
 
 import force.ssafy.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 //    Optional<Member> findByNickname(String nickname);
     boolean existsBySolvedAcId(String solvedAcId);
     Optional<Member> findBySolvedAcId(String solvedAcId);
+    Page<Member> findByVerified(boolean verified, Pageable pageable); // 인증된 회원 페이징 조회 (배치 Reader용)
 }
