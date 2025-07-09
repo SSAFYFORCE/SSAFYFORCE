fda9077a3053fb22ad7913228f40ac1c204d2f07
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-07-09 11:22:29 +0900

    chore: 안쓰는 import 지우기

--- backend/src/main/java/force/ssafy/domain/solvedProblem/batch/SolvedProblemSyncScheduler.java
+++ backend/src/main/java/force/ssafy/domain/solvedProblem/batch/SolvedProblemSyncScheduler.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
